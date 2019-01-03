a6937086ea3f960e3c3af534f658f51b8bb53828
Process My Media <mnicolescu@gmail.com>
2019-01-03 20:22:21 +0100

    Add EthereumCore.App

---
+++ EthereumCore.App/AppService.cs
@@ -0,0 +1,60 @@
+﻿namespace EthereumCore.App
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// App Service
+    /// </summary>
+    /// <seealso cref="Microsoft.Extensions.Hosting.IHostedService" />
+    /// <seealso cref="System.IDisposable" />
+    public class AppService : IHostedService, IDisposable
+    {
+        private ILogger logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AppService"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        public AppService(ILogger<AppService> logger)
+        {
+            this.logger = logger;
+        }
+
+        /// <summary>
+        /// Triggered when the application host is ready to start the service.
+        /// </summary>
+        /// <param name="cancellationToken">Indicates that the start process has been aborted.</param>
+        /// <returns></returns>
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation("App started");
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Triggered when the application host is performing a graceful shutdown.
+        /// </summary>
+        /// <param name="cancellationToken">Indicates that the shutdown process should no longer be graceful.</param>
+        /// <returns></returns>
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            this.logger.LogInformation("App stopped");
+
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+
+        }
+    }
+}
---
+++ EthereumCore.App/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace EthereumCore.App
+{
+    using System.Threading.Tasks;
+
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    /// <summary>
+    /// Program entry point
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Mains the specified arguments.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
+        /// <returns></returns>
+        public static async Task Main(string[] args)
+        {
+            var builder = new HostBuilder()
+                  .ConfigureAppConfiguration((hostingContext, config) =>
+                  {
+                      config.AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+                      config.AddJsonFile($"appsettings.override.json", optional: true, reloadOnChange: true);
+
+                  })
+                  .ConfigureServices((hostContext, services) =>
+                  {
+                      services.AddOptions();
+                      services.AddSingleton<IHostedService, AppService>();
+
+                  })
+                  .ConfigureLogging((hostingContext, logging) => {
+                      logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                      logging.AddConsole();
+                  });
+
+            await builder.RunConsoleAsync();
+        }
+    }
+}
